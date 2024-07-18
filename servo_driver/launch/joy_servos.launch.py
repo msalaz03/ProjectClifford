5c3c8d7ba31cad9740969bd7d731edca3ecc00cf
msalaz03 <msalaz03@uogeulph.ca>
2024-07-18 19:54:28 -0400

    nodes init for servos

---
+++ servo_driver/launch/joy_servos.launch.py
@@ -0,0 +1,41 @@
+import os   
+
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time') #look more into
+    
+    joy_params = os.path.join(get_package_share_directory('teleop_controller'),'config','joystick.yaml')
+    
+    #launch joy node.
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        parameters=[
+            joy_params, 
+            {'use_sim_time':use_sim_time}
+            ]
+    )
+
+    servo_node = Node(
+        package='servo_driver',
+        executable='servo_pca9685',
+    )
+
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                'use_sim_time',
+                default_value='false',
+                description='use sim time if true'
+            ),
+            joy_node,
+            servo_node,
+        ]
+    )
--- servo_driver/pca9685_nodes/servo_pca9685.py
+++ servo_driver/pca9685_nodes/servo_pca9685.py
@@ -12,6 +12,7 @@ from adafruit_motor import servo
 import RPi.GPIO as GPIO
 
 #IMPORT KINEMATIC LIBRARIES
+import math as math
 
 #GENERAL IMPORTS
 from time import sleep as sleep
@@ -31,7 +32,7 @@ class ServoDriver(Node):
         self.get_logger().info('Clifford Servo Driver Online...')
 
         #Declare our Publishers & Subscribers
-        self.clifford_joy_sub = self.create_subscription( Joy, 'joy', self.clifford_joystick_callback)
+        self.clifford_joy_sub = self.create_subscription( Joy, 'joy', self.clifford_joystick_callback,1)
 
         #ServoDriver Parameters
         self.declare_parameter('min_pulse', 600) # As specified per Datasheet.
@@ -51,31 +52,53 @@ class ServoDriver(Node):
         self.servo1 = servo.Servo(pca.channels[1])
 
         #INIT PULSE PARAMETERS
-        self.servo0.set_pulse_width_range( self.get_parameter('min_pulse'),
-                                      self.get_parameter('max_pulse') )
-        self.servo1.set_pulse_width_range( self.get_parameter('min_pulse'),
-                                      self.get_parameter('max_pulse') )
+        self.servo0.set_pulse_width_range( 600,2400)
+        self.servo1.set_pulse_width_range( 600,2400)
+
+        #INIT SERVO RELATIVE COODS + MISC
+        self.universal_shoulder_len = 58.17
+        self.universal_arm_len = 107.00
+        self.universal_wrist_len = 130.43
+
+        self.test_coords =  [36.1,0.0,157.3]
+        self.ref_coords= [0.0, 0.0, 0.0]
+        
+
+
 
     def clifford_joystick_callback(self, data):
-        self.get_logger().info('Clifford Joystick Callback')
+        #self.get_logger().info('Clifford Joystick Callback')
 
         # X button condition
         if data.buttons[0] == 1:
             self.get_logger().info('X Pressed...')
+            self.zero_servos()
 
         # Circle button condition
         elif data.buttons[1] == 1:
             self.get_logger().info("Circle Pressed...")
+            self.test_servos()
 
         # Triangle button condition
         elif data.buttons[2] == 1:
             self.get_logger().info("Triangle Pressed...")
+            theta_2, theta_3 = self.solve_ik(self.test_coords)
+            self.get_logger().info(f'THETA_2: {theta_2}')
+            self.get_logger().info(f'THETA_2: {theta_3}')
+
           
         # Square button condition
         elif data.buttons[3] == 1:
             self.get_logger().info('Sqaure Pressed...')
             
-    
+    def zero_servos(self):
+        self.get_logger().info("Helper Function: 'zero_servos' called")
+        sleep(1)
+        self.servo0.angle = 0
+        sleep(2)
+        self.servo0.angle = 90
+
+
     # Helper function to test servo range of motion (FINISH IMPLEMENTATION ONCE REST ARE INSTALLED).
     def test_servos(self): 
         self.get_logger().info("Helper Function: 'test_servos' called.")
@@ -87,6 +110,51 @@ class ServoDriver(Node):
         self.servo0.angle = 90 # Move to 90 degrees (technically 0)
     
     #DEFINE NEW FUNC RESET SERVOS
+
+    def solve_ik(self,cords):
+            # These kinematics calculations will try to be as descripitional as possible but please refer
+            # to sheet of calculations by Cameron Bauman. 
+            #UNIT: RADIANS & mm
+            x_cord = cords[0] #x cord value
+            y_cord = cords[1] #y cord value not really relevant rn.
+            z_cord = cords[2] #z cord value
+
+            #Find length B using Pythagorean's Theorem
+            b_len = math.sqrt( pow(x_cord,2) + pow(z_cord,2) ) 
+            
+            # Angle of B
+            beta_1 = math.atan(z_cord/x_cord)
+            
+            #Calculations for 'right_arm' and 'right_wrist' applied through cosine law.
+
+            #This is the angle of which right_arm is set. This is necessary for calculating how the long will be SET
+            beta_2 = math.acos( ( pow(self.universal_arm_len,2) + pow(b_len,2) - pow(self.universal_wrist_len,2) ) 
+                                    / (2 * self.universal_arm_len * b_len) )
+
+            #This is the angle of which right_wrist is set.
+            beta_3 = math.acos( ( pow(self.universal_arm_len,2) + pow(self.universal_wrist_len,2) - pow(b_len,2) ) 
+                                    / (2 * self.universal_arm_len * self.universal_wrist_len) )
+            
+            #Shouldn't be too relevant to calculations besides for RVIZ, but this is to make the calculations relative to their axes.
+            b_prime = (math.pi  * 2) - beta_1
+            theta_2 = b_prime - beta_2
+            theta_2 = theta_2 - math.pi
+
+
+            #THIS VALUE IS HARD CODED
+            theta_2 = (math.pi/4) - theta_2  #Final value of right_arm 
+
+            theta_3 = math.pi - beta_3
+            theta_3 = (math.pi/2) - theta_3 #Final value of right_wrist
+
+            # theta_2 = theta_2 * (180/math.pi)
+            # theta_3 = theta_3 * (180/math.pi)
+            
+            return [theta_2,theta_3]
+
+    
+
+
 
 
 def main(args=None):
